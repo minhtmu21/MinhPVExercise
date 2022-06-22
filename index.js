217d2681659af30771d3436f8425beed3df49856
minhtmu <minhtmu@gmail.com>
2022-06-23 02:05:37 +0700

    day 23

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const loginRouter = require("./login");
-const registerRouter = require("./register");
 const handlebars = require("express-handlebars")
 const fs = require("fs");
+const db = require("./db");
 const session = require('express-session')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(bodyParser.json())
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const connectionString = 'mongodb+srv://minhtmu:minhminh@cluster0.zsj9s.mongodb.net/?retryWrites=true&w=majority';
 
+mongoose.connect(connectionString);
+
+const loginRequest = new mongoose.Schema({
+  username: {
+    type: String,
+    trim: true
+  },
+  passwork: {
+    type: String
+  }
+})
+
+const LoginModel = mongoose.model('Acc', loginRequest);
+const {
+  nextTick
+} = require("process");
 
 const hbs = handlebars.create({
   helpers: {
@@ -14,6 +37,82 @@ const hbs = handlebars.create({
       return moment.unix(+timestamp).format("DD-MM-YYYY HH:mm:ss")
     }
   }
+})
+
+const userSchema = Joi.object({
+  username: Joi.string().min(3).max(20).trim().required(),
+  passwork: Joi.string().min(6).max(20).required()
+})
+
+let addUser
+app.post('/register', async (req, res, next) => {
+  console.log(req)
+  const {
+    username,
+    passwork
+  } = req.body
+  try {
+    const value = await userSchema.validateAsync({
+      username: username,
+      passwork: passwork
+    });
+    next()
+  } catch (err) {
+    return res.status(401).json({
+      message: err.message
+    })
+  }
+}, async (req, res) => {
+  console.log(req)
+  const {
+    username,
+    passwork
+  } = req.body
+  const result = await LoginModel.findOne({
+    username: username
+  })
+  console.log(result)
+  if (result) {
+    return res.status(401).json({
+      message: "Tài khoản đã tồn tại"
+    })
+  } else {
+    addUser = new LoginModel({
+      username: username,
+      passwork: passwork
+    });
+    addUser.save().then()
+    return res.status(200).json({
+      message: "Đăng ký thành công"
+    })
+  }
+})
+
+app.post('/login', async (req, res, next) => {
+  try {
+    const value = await userSchema.validateAsync(req.body);
+    next()
+  } catch (err) {
+    return res.status(401).json({
+      message: err.message
+    })
+  }
+}, async (req, res) => {
+  const {
+    username,
+    passwork
+  } = req.body
+  const result = await LoginModel.findOne({
+    username: username,
+    passwork: passwork
+  })
+  if (result) {
+    return res.status(200).json({
+      message: "Đăng nhập thành công"
+    })
+  } else return res.status(401).json({
+    message: "Tài khoản hoặc mật khẩu không đúng"
+  })
 })
 
 app.engine('handlebars', hbs.engine);
@@ -29,33 +128,6 @@ app.use(session({
     secure: true
   }
 }))
-
-app.use("/login", loginRouter)
-app.use("/register", registerRouter)
-
-function isAuthenticated(req, res, next) {
-  if (req.session.isLoggedIn) next()
-  else res.redirect('login')
-}
-
-app.get('/logout', function (req, res, next) {
-  req.session.isLoggedIn = 0
-  req.session.save(function (err) {
-    if (err) next(err)
-    req.session.regenerate(function (err) {
-      if (err) next(err)
-      res.redirect('/')
-    })
-  })
-})
-
-app.get('/', isAuthenticated, function (req, res) {
-  return res.redirect("homePage")
-})
-
-app.get('/homePage', isAuthenticated, function (req, res) {
-  return res.redirect('login')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
